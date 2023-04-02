409c5f6e76c50a711e87787a0a7957073470e4ea
Maiiiiiid <zhp92@outlook.com>
2023-04-02 11:54:16 +0800

    feat: (MsgInput) add the onSend callback function

---
+++ lib/comp/msg_input_comp.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class MsgInput extends StatefulWidget {
+  const MsgInput({super.key, required this.onSend});
+
+  final ValueChanged<String> onSend;
+
+  @override
+  State<StatefulWidget> createState() {
+    return MsgInputState();
+  }
+}
+
+class MsgInputState extends State<MsgInput> {
+  final TextEditingController textEditingController = TextEditingController();
+  final FocusNode focusNode = FocusNode();
+
+  String msg = '';
+
+  void sendMessage() {
+    widget.onSend(msg);
+    msg = '';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Border border = const Border(
+      top: BorderSide(color: Color.fromARGB(255, 223, 223, 223), width: 0.5),
+      bottom: BorderSide(color: Color.fromARGB(255, 223, 223, 223), width: 0.1),
+    );
+
+    return Container(
+      width: double.infinity,
+      height: 50,
+      decoration: BoxDecoration(
+        border: border,
+        color: Colors.white,
+        // borderRadius: const BorderRadius.all(Radius.circular(0.1)),
+      ),
+      child: Row(
+        children: <Widget>[
+          const Spacer(
+            flex: 1,
+          ),
+          Flexible(
+            flex: 13,
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  msg = value;
+                });
+              },
+              style: const TextStyle(color: Colors.grey, fontSize: 14),
+              controller: textEditingController,
+              decoration: const InputDecoration.collapsed(
+                hintText: 'Type your message...',
+                hintStyle: TextStyle(color: Colors.grey),
+              ),
+              focusNode: focusNode,
+              autofocus: true,
+            ),
+          ),
+          // Button send message
+          Material(
+            color: Colors.white,
+            child: Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 8,
+              ),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: sendMessage,
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:maid_gpt/comp/bubble_comp.dart';
-import 'package:maid_gpt/comp/input_comp.dart';
+import 'package:maid_gpt/comp/msg_input_comp.dart';
 
 import '../models/message_chat.dart';
 
@@ -32,6 +32,21 @@ class _ChatPageState extends State<ChatPage> {
         buildBubbleWidget('xxxxxxxxxxxxxxxxxxxx', Role.maid),
         buildBubbleWidget('xxxxxxxxxxxxxxxxxxxxxxxx', Role.user),
         buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
+        buildBubbleWidget('xxxxx', Role.maid),
         buildBubbleWidget(
             'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
             Role.maid),
@@ -40,30 +55,39 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   Widget buildInputWidget() {
-    return const InputMsgBox();
+    return MsgInput(
+      onSend: (String value) {
+        // TODO 添加一个气泡
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.onPrimary,
-          title: const Center(
-            child: Text('test'),
-          ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.onPrimary,
+        title: const Center(
+          child: Text('test'),
         ),
-        body: SafeArea(
-          child: Stack(
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  buildBubbleListWidget(),
-                  buildInputWidget(),
-                  // buildInputWidget(),
-                ],
-              )
-            ],
-          ),
-        ));
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                buildBubbleListWidget(),
+                const SizedBox(
+                  height: 60,
+                )
+                // buildInputWidget(),
+                // buildInputWidget(),
+              ],
+            )
+          ],
+        ),
+      ),
+      bottomSheet: buildInputWidget(),
+    );
   }
 }
