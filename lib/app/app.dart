dcd3056f1272c1008d04d235a1302b5cb19779da
Maiiiiiid <zhp92@outlook.com>
2023-04-23 23:32:08 +0800

    refactor: add routers and pages

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:maid_gpt/app/routers/app_pages.dart';
 import 'package:maid_gpt/app/routers/app_routers.dart';
 
 class App extends StatefulWidget {
--- lib/app/routers/app_routers.dart
+++ lib/app/routers/app_routers.dart
@@ -1,6 +1,17 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+
+import '../pages/home_page.dart';
+import '../pages/conversation_page.dart';
+
 class AppRouter{
   static const home = '/';
   static const settings = '/settings';
   static const service = '/service';
   static const conversation = '/conversation';
 }
+
+List<GetPage> appPages = [
+  GetPage(name: AppRouter.home, page: () => const HomePage()),
+  GetPage(name: AppRouter.conversation, page: () => const ConversationPage()),
+];
+
