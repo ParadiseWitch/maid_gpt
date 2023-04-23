2cd4f36bf3edc6d174ce60d23598127778f6d6bb
Maiiiiiid <zhp92@outlook.com>
2023-04-23 23:13:20 +0800

    refactor: wip - add the 'get' dependency

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,18 +1,27 @@
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 class App extends StatefulWidget {
-  const App({Key? key}): super(key: key);
+  const App({Key? key}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _AppState();
-
 }
 
-class _AppState extends State<App>{
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return const Text("");
+    return GetMaterialApp(
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.system,
+      initialRoute: '/',
+      initialBinding: null,
+      getPages: [GetPage(name: 'home', page: () => const Text(''))],
+      locale: const Locale('zh', 'CN'),
+      builder: null,
+      debugShowCheckedModeBanner: false,
+    );
   }
-
 }
